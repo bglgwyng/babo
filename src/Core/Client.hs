5be5421cae559dee575b5f7098776544b3179067
bglgwyng <bglgwyng@gmail.com>
2021-12-30 14:53:28 +0900

    refactor: remove unused functions

--- src/Core/Client.hs
+++ src/Core/Client.hs
@@ -67,45 +67,6 @@ typeOf gcxt mcxt cxt t =
   where
     typeOf' = typeOf gcxt
 
-fill ::
-  GlobalContext ->
-  M.Map Id Term ->
-  M.Map Id Term ->
-  Term ->
-  UnifyM (S.Set Constraint)
-fill gcxt mcxt cxt t =
-  case t of
-    Ap l r -> do
-      (tpl, cl) <- typeOf' mcxt cxt l
-      case tpl of
-        Pi from to -> do
-          optional (typeOf' mcxt cxt r)
-            <&> (cl <>) . foldMap (\(tpr, cr) -> cr <> S.singleton (from, tpr))
-        t -> error $ show t
-    Lam arg b -> do
-      v <- lift gen
-      args' <- fill gcxt mcxt cxt arg
-      cs <-
-        fill
-          gcxt
-          mcxt
-          (M.insert v arg cxt)
-          (subst (Free v) 0 b)
-      pure $ args' <> cs
-    Pi from to -> do
-      v <- lift gen
-      maybeFromUnification <- fill gcxt mcxt cxt from
-      (toTp, toCs) <- typeOf' mcxt (M.insert v from cxt) (subst (Free v) 0 to)
-      return
-        ( maybeFromUnification
-            <> toCs
-            -- <> foldMap (S.singleton . (Uni,) . fst) maybeFromUnification
-            <> S.singleton (Type, toTp)
-        )
-    _ -> mzero
-  where
-    typeOf' = typeOf gcxt
-
 infer :: GlobalContext -> Term -> Maybe (Term, Subst, S.Set Constraint)
 infer gcxt t = listToMaybe . runUnifyM $ go
   where
@@ -113,14 +74,6 @@ infer gcxt t = listToMaybe . runUnifyM $ go
       (tp, cs) <- typeOf gcxt M.empty M.empty t
       (subst, flexflex) <- unify Context {metas = M.empty} cs
       return (manySubst subst tp, subst, cs)
-
-fill' :: GlobalContext -> Term -> Maybe Term
-fill' gcxt t = listToMaybe . runUnifyM $ go
-  where
-    go = do
-      (tp, cs) <- typeOf gcxt M.empty M.empty t
-      (subst, flexflex) <- unify Context {metas = M.empty} cs
-      return (manySubst subst tp)
 
 infer' :: GlobalContext -> M.Map Id Term -> Term -> Term -> Maybe (Term, Term)
 infer' gctx ctx t1 t2 = listToMaybe . runUnifyM $ go
